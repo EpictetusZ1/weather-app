c2c4cce6bb863cf30a4626755c20f1202ba6d6aa
J Heaton <jackrobertheaton@gmail.com>
2021-10-18 14:42:27 -0400

    Simplify error handling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./styles/style.css"
 import mainSection from "./modules/createMain";
 import Card from "./modules/makeCard";
 import getData from "./modules/getData";
+import apiCall from "./modules/getData";
 
 let cityArr = []
 
@@ -18,7 +19,7 @@ loadDisplay()
 
 const checkLocalStore = () => {
     if (!localStorage.getItem("cities")) {
-        return cityArr.push("Toronto", "New York")
+        return cityArr.push("Toronto")
     } else {
         return cityArr = JSON.parse(localStorage.getItem("cities"))
     }
@@ -34,16 +35,37 @@ const handleInput = () => {
         localStorage.setItem("cities", JSON.stringify(arr))
     }
 
+    const alertError = (text) => {
+        let msg
+        if (text === "400") msg = "Please enter a city"
+        if (text === "404") msg = "City not found"
+        let errMsg = document.createElement("p")
+        errMsg.className = "errMsg"
+        errMsg.textContent = msg
+        searchForm.appendChild(errMsg)
+
+        searchText.classList.add("alertErr")
+
+        setTimeout(() => {
+            searchText.classList.toggle("alertErr")
+            searchForm.removeChild(errMsg)
+        }, 2000)
+    }
+
     const errorFlow = async() => { // Here we make the API call to see if it returns a valid response
-        const response = await getData.makeCall(searchText.value)
-        cityArr.push(response["name"])
-        setLocal(cityArr)
-        return response
+        try {
+            const response = await getData.makeCall(searchText.value)
+            cityArr.push(response["name"])
+            setLocal(cityArr)
+            Card.makeCard(response)
+        } catch (error) {
+            alertError(error.message)
+        }
     }
 
     searchForm.addEventListener("submit", (e) => {
         e.preventDefault()
-        errorFlow().then((response) => Card.makeCard(response))
+        errorFlow()
         searchForm.reset()
     })
 }
@@ -57,4 +79,3 @@ const addCards = () => {
     }
 }
 addCards()
-
--- src/modules/getData.js
+++ src/modules/getData.js
@@ -2,28 +2,19 @@ import env from "../../config"; // Mock env var for OpenWeather API
 
 const apiCall = (()=> {
 
-    const makeCall = async(city) => {
-        try {
-            const api = `api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${env.apiKey}`
-            const response = await fetch(`http://${api}`, {mode: "cors"})
-            const weather = await response.json()
-            console.log(weather)
-            if (weather.cod === "400") throw {
-                msg: "HTTP: 400 Bad Request Error",
-                usrMsg: "Please enter a city",
-                code: 400
-            }
-            if (weather.cod === "404") throw {
-                msg: "HTTP: 404 Not found",
-                msgUsr: "City not found",
-                code: 404
-            }
-            if (weather.cod === 200) return weather
-        } catch (error) {
-            console.log(error)
-            return {error}
-        }
+    const myError = (code) => {
+        throw new Error(`${code}`)
+    }
 
+    const makeCall = async(city) => {
+        const api = `api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${env.apiKey}`
+        const response = await fetch(`http://${api}`, {mode: "cors"})
+        if (response.ok) {
+            const weather = await response.json()
+            return weather
+        } else {
+            return myError(response.status)
+        }
     }
     return {
         makeCall
